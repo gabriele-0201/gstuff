a71cf75d8893c047d5976b23dfb040ded1d432b4
Gabriele <gabriele-0201>
2022-04-17 15:58:13 +0200

    working on fonts

--- x11yt.cpp
+++ x11yt.cpp
@@ -33,12 +33,11 @@ struct Point {
     int x, y;
 } corner;
 
-std::string strText;
-
 void init();
 void close();
 unsigned long RGB(int r, int g, int b);
 void loadConfig(std::string name);
+void set_up_font ();
 
 int main() {
 
@@ -48,18 +47,17 @@ int main() {
     style.borderColor = RGB(255, 255, 255);
     style.duration = 1000;
     style.padding = 4;
-    style.border = 2;
+    style.border = 5;
     style.position = Pos::TOP_LEFT;
-
-    strText = "prova";
 
     init();
     XEvent event;
+    char text[255];
 
     while (1)
     {
         XNextEvent(dis, &event);
-        if(event.type==ButtonPress)
+        if(event.type==ButtonPress) 
             close();
     }
     
@@ -77,7 +75,7 @@ void init() {
     blue=(0,0,255);
 
     // calc dimension of the window
-    int wWidth = 100, wHeight = 100;
+    int wWidth = 300, wHeight = 200;
     
     // calc corner near angle
     Screen*  s = DefaultScreenOfDisplay(dis);
@@ -100,10 +98,10 @@ void init() {
     }
 
     // Calc size text
-    XTextItem text;
-    text.chars = &strText[0];
-    text.nchars = strText.length();
-    text.delta = 1;
+    //XTextItem text;
+    //text.chars = &strText[0];
+    //text.nchars = strText.length();
+    //text.delta = 1;
     //text.font = None;
     
     // Create the window
@@ -117,10 +115,6 @@ void init() {
     XSetBackground(dis,gc,white);
     XSetForeground(dis,gc,black);
 
-    // draw text
-    //XDrawText(dis, win, gc, 0, 0, strText, strText.length());
-    XDrawString(dis, win, gc, 5, 5, &strText[0], strText.length());
-    
     // Set non managed window
     XSetWindowAttributes set_attr;
     set_attr.override_redirect = True;
@@ -132,6 +126,28 @@ void init() {
     // on top
     XClearWindow(dis, win);
     XMapRaised(dis, win);
+    
+    // draw text
+    set_up_font();
+
+    XSetForeground(dis,gc,white);
+    char * txt = "ajkhasdjkh";
+    XDrawString(dis, win, gc, 50, 50, txt, strlen(txt));
+
+}
+
+/* Set up the text font. */
+
+void set_up_font ()
+{
+    const char * fontname = "-*-Roboto Condensed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1";
+    XFontStruct * font = XLoadQueryFont (dis, fontname);
+    /* If the font could not be loaded, revert to the "fixed" font. */
+    if (! font) {
+        fprintf (stderr, "unable to load font %s: using fixed\n", fontname);
+        font = XLoadQueryFont (dis, "fixed");
+    }
+    XSetFont (dis, gc, font->fid);
 }
 
 void close() {
